fdfaedbb3b35327a7334006ad96f56964d5b06a3
Zheka Horunzhii <horunzhy.wgen@gmail.com>
2021-02-01 23:01:17 +0200

    Finished with forms exploration

--- todo/src/components/item-add-form/item-add-form.js
+++ todo/src/components/item-add-form/item-add-form.js
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './item-add-form.css';
 
-const ItemAddForm = ({createToDoItem}) => {
-    return (
-        <div className='item-add-form'>
-            <button
-                className='btn btn-outline-secondary'
-                onClick={() => createToDoItem('Get Better')}>Add Item</button>
-        </div>
-    );
-};
+export default class ItemAddForm extends Component {
+    state = {
+        label: ''
+    }
 
-export default ItemAddForm;
+    onLabelChange = (event) => {
+        this.setState({
+            label: event.target.value
+        });
+    }
+
+    onSubmit = (event) => {
+        event.preventDefault();
+
+        this.props.createToDoItem(this.state.label);
+
+        this.setState({
+            label: ''
+        });
+    }
+
+    render() {
+        return (
+            <form className='item-add-form d-flex'
+                onSubmit={this.onSubmit}>
+
+                <input  type='text'
+                        className='form-control'
+                        onChange={this.onLabelChange}
+                        placeholder='What needs to be done?'
+                        value={this.state.label} />
+    
+                <button
+                    className='btn btn-outline-secondary'>Add Item</button>
+            </form>
+        );
+    }
+};
